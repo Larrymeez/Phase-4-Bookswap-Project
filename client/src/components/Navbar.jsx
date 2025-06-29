be9a60a9366fda2b73e3ff438c9e686e5a203250
Larrymeez <meezlarry@gmail.com>
2025-06-29 17:02:00 +0300

    creates a navbar page

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Navbar = () => {
+  return (
+    <nav className="bg-gray-100 px-6 py-4 mb-4 shadow">
+      <ul className="flex gap-4">
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/signup">Sign Up</Link></li>
+        <li><Link to="/login">Log In</Link></li>
+      </ul>
+    </nav>
+  )
+}
+
+export default Navbar
