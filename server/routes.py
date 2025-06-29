96e4b0b285b14f3d9bf032ce37b86393da44691c
Larrymeez <meezlarry@gmail.com>
2025-06-29 15:42:13 +0300

    Add POST /books route to create new books with validation

--- server/routes.py
+++ server/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
 from models import db, Book
 
 api = Blueprint('api', __name__)
@@ -18,3 +18,38 @@ def get_books():
         for book in books
     ]
     return jsonify(book_list), 200
+
+
+@api.route('/books', methods=['POST'])
+def create_book():
+    data = request.get_json()
+
+    title = data.get('title')
+    author = data.get('author')
+    genre = data.get('genre')
+    condition = data.get('condition')
+    owner_id = data.get('owner_id')  # will be replaced with JWT user ID later
+
+    if not all([title, author, owner_id]):
+        return {'error': 'Title, author, and owner_id are required.'}, 400
+
+    new_book = Book(
+        title=title,
+        author=author,
+        genre=genre,
+        condition=condition,
+        owner_id=owner_id
+    )
+
+    db.session.add(new_book)
+    db.session.commit()
+
+    return {
+        'id': new_book.id,
+        'title': new_book.title,
+        'author': new_book.author,
+        'genre': new_book.genre,
+        'condition': new_book.condition,
+        'owner_id': new_book.owner_id
+    }, 201
+
