44a7f77f9ab7d86dd1a48ce7b3b8c31490a50a6c
Larrymeez <meezlarry@gmail.com>
2025-06-29 13:06:04 +0300

    Set up routes

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import App from './App.jsx'
+import BooksPage from './pages/BooksPage.jsx'
+import ClubsPage from './pages/ClubsPage.jsx'
+import ProfilePage from './pages/ProfilePage.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/books" element={<BooksPage />} />
+        <Route path="/clubs" element={<ClubsPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>,
 )
