c83719498eabe5943f8239abadd2aab16924a880
Larrymeez <meezlarry@gmail.com>
2025-06-29 17:01:01 +0300

    updates main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,18 +1,29 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
 import App from './App.jsx'
 import Signup from './pages/Signup.jsx'
 import Login from './pages/Login.jsx'
+import Navbar from './components/Navbar.jsx'
+
+const RootLayout = ({ children }) => (
+  <div>
+    <Navbar />
+    <main className="p-6">{children}</main>
+  </div>
+)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/login" element={<Login />} />
-      </Routes>
+      <RootLayout>
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </RootLayout>
     </BrowserRouter>
   </React.StrictMode>
 )
