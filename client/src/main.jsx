b0d712dab13d44b31b3ab25f7a5d01e2922916ae
Larrymeez <meezlarry@gmail.com>
2025-06-30 07:53:01 +0300

    Wrap app in AuthProvider

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -7,7 +7,7 @@ import Signup from './pages/Signup.jsx'
 import Login from './pages/Login.jsx'
 import Navbar from './components/Navbar.jsx'
 import MyBooks from './pages/MyBooks.jsx'
-
+import { AuthProvider } from './context/AuthContext'
 
 const RootLayout = ({ children }) => (
   <div>
@@ -15,18 +15,19 @@ const RootLayout = ({ children }) => (
     <main className="p-6">{children}</main>
   </div>
 )
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      <RootLayout>
-        <Routes>
-          <Route path="/" element={<App />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/my-books" element={<MyBooks />} />
-        </Routes>
-      </RootLayout>
+      <AuthProvider>
+        <RootLayout>
+          <Routes>
+            <Route path="/" element={<App />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/my-books" element={<MyBooks />} />
+          </Routes>
+        </RootLayout>
+      </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>
 )
