f75b7d4d3921dcbe44d686338f39197fc424ecf6
Larrymeez <meezlarry@gmail.com>
2025-06-30 08:23:27 +0300

    Adds logout functionality in Navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,18 +1,34 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Link } from 'react-router-dom'
+import { AuthContext } from '../context/AuthContext'
 
 const Navbar = () => {
+  const { user, logout } = useContext(AuthContext)
+
   return (
     <nav className="bg-gray-100 px-6 py-4 mb-4 shadow">
-      <ul className="flex gap-4">
+      <ul className="flex gap-4 items-center">
         <li><Link to="/">Home</Link></li>
-        <li><Link to="/signup">Sign Up</Link></li>
-        <li><Link to="/login">Log In</Link></li>
-        <li><Link to="/my-books">My Books</Link></li>
 
+        {user ? (
+          <>
+            <li><Link to="/my-books">My Books</Link></li>
+            <li>
+              <button onClick={logout} className="bg-red-500 text-white px-3 py-1 rounded">
+                Logout
+              </button>
+            </li>
+          </>
+        ) : (
+          <>
+            <li><Link to="/signup">Sign Up</Link></li>
+            <li><Link to="/login">Log In</Link></li>
+          </>
+        )}
       </ul>
     </nav>
   )
 }
 
 export default Navbar
+
